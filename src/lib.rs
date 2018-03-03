ee1e603fc68e5bb99662ff55eef1b26c3ec068be
Sven Strittmatter <sven.strittmatter@iteratec.de>
2018-03-04 00:32:24 +0100

    Implement a game status

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@
 #[macro_use]
 extern crate hamcrest;
 
+use std::fmt;
+
 /// A cell should die if it has less than two or more than three neighbours.
 fn should_die(number_of_neighbours: u32) -> bool {
     match number_of_neighbours {
@@ -14,6 +16,35 @@ fn should_die(number_of_neighbours: u32) -> bool {
 /// neighbour.
 fn should_spawn(number_of_neighbours: u32) -> bool {
     number_of_neighbours == 3
+}
+
+#[derive(PartialEq)]
+pub struct Status {
+    iteration: u32,
+    cells: u32,
+    born: u32,
+    died: u32,
+}
+
+impl Status {
+    pub fn new(iteration: u32, cells: u32, born: u32, died: u32) -> Status {
+        Status { iteration, cells, born, died }
+    }
+
+    pub fn stringify(&self) -> String {
+        format!(
+            "Iteration: {}, Cells: {}, Born: {}, Died: {}",
+            self.iteration,
+            self.cells,
+            self.born,
+            self.died)
+    }
+}
+
+impl fmt::Display for Status {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.stringify())
+    }
 }
 
 #[cfg(test)]
@@ -109,5 +140,14 @@ mod tests {
     #[test]
     fn should_not_spawn_new_cell_if_eight_neighbours() {
         assert_that!(should_spawn(8), is(false));
+    }
+
+    #[test]
+    fn format_game_status() {
+        let status = Status::new(42, 23, 5, 3);
+
+        assert_that!(
+            status.stringify(),
+            is(equal_to(String::from("Iteration: 42, Cells: 23, Born: 5, Died: 3"))));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
+#[cfg(test)]
+#[macro_use]
+extern crate hamcrest;
 extern crate game_of_live;
+
+use game_of_live::Status;
 
 fn main() {
     println!("Game of Live");
+    println!("============");
+    println!();
+    println!("{}", Status::new(42, 23, 5, 3));
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use hamcrest::prelude::*;
+
 }

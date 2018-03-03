12814d5cca1f1f032ada1fbd5b76687609d18616
Sven Strittmatter <sven.strittmatter@iteratec.de>
2018-03-04 00:02:37 +0100

    Use the hamcrest macro

--- src/lib.rs
+++ src/lib.rs
@@ -23,27 +23,27 @@ mod tests {
 
     #[test]
     fn cell_must_die_if_zero_neighbours() {
-        assert_that(should_die(0), is(true));
+        assert_that!(should_die(0), is(true));
     }
 
     #[test]
     fn cell_must_die_if_one_neighbours() {
-        assert_that(should_die(1), is(true));
+        assert_that!(should_die(1), is(true));
     }
 
     #[test]
     fn cell_must_not_die_if_two_neighbours() {
-        assert_that(should_die(2), is(false));
+        assert_that!(should_die(2), is(false));
     }
 
     #[test]
     fn cell_must_not_die_if_three_neighbours() {
-        assert_that(should_die(3), is(false));
+        assert_that!(should_die(3), is(false));
     }
 
     #[test]
     fn cell_must_die_if_four_neighbours() {
-        assert_that(should_die(4), is(true));
+        assert_that!(should_die(4), is(true));
     }
 
     #[test]

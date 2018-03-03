1595cd661b83e297c71727c1c1365390ef564642
Sven Strittmatter <sven.strittmatter@iteratec.de>
2018-03-04 00:01:35 +0100

    Implement rule when to spawn a new cell on an empty place

--- src/lib.rs
+++ src/lib.rs
@@ -2,17 +2,24 @@
 #[macro_use]
 extern crate hamcrest;
 
+/// A cell should die if it has less than two or more than three neighbours.
+fn should_die(number_of_neighbours: u32) -> bool {
+    match number_of_neighbours {
+        2 | 3 => false,
+        _ => true,
+    }
+}
+
+/// At an empty place a new cell should spawn, if this cell has exactly three living cells as
+/// neighbour.
+fn should_spawn(number_of_neighbours: u32) -> bool {
+    number_of_neighbours == 3
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use hamcrest::prelude::*;
-
-    fn should_die(number_of_neighbours: u32) -> bool {
-        match number_of_neighbours {
-            2 | 3 => false,
-            _ => true,
-        }
-    }
 
     #[test]
     fn cell_must_die_if_zero_neighbours() {
@@ -37,5 +44,40 @@ mod tests {
     #[test]
     fn cell_must_die_if_four_neighbours() {
         assert_that(should_die(4), is(true));
+    }
+
+    #[test]
+    fn should_not_spawn_new_cell_if_zero_neighbours() {
+        assert_that!(should_spawn(0), is(false));
+    }
+
+    #[test]
+    fn should_not_spawn_new_cell_if_one_neighbours() {
+        assert_that!(should_spawn(1), is(false));
+    }
+
+    #[test]
+    fn should_not_spawn_new_cell_if_two_neighbours() {
+        assert_that!(should_spawn(2), is(false));
+    }
+
+    #[test]
+    fn should_spawn_new_cell_if_three_neighbours() {
+        assert_that!(should_spawn(3), is(true));
+    }
+
+    #[test]
+    fn should_not_spawn_new_cell_if_four_neighbours() {
+        assert_that!(should_spawn(4), is(false));
+    }
+
+    #[test]
+    fn should_not_spawn_new_cell_if_five_neighbours() {
+        assert_that!(should_spawn(5), is(false));
+    }
+
+    #[test]
+    fn should_not_spawn_new_cell_if_six_neighbours() {
+        assert_that!(should_spawn(6), is(false));
     }
 }

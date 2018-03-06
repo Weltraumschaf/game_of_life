6102e89a814a30d4d25f9071a44e930a30d3c78a
Sven Strittmatter <sven.strittmatter@iteratec.de>
2018-03-06 22:02:28 +0100

    Doccument the main

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,11 @@ use game_of_live::population::Population;
 use game_of_live::cell::Cell;
 use game_of_live::place::Place;
 
+/// The main entry point of the binary.
+///
+/// This create follows the paradigm of an executable library: This means that the whole application
+/// is in the library part of the crate (and so can be used as a dependency). The main module only
+/// provides the main function with simple code which delegates to the library part.
 fn main() {
     println!("Game of Live");
     println!("============");

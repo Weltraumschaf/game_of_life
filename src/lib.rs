ea998dd3224335a463da64c1422eae784560089b
Sven Strittmatter <sven.strittmatter@iteratec.de>
2018-03-06 09:30:39 +0100

    Implement some vector math to calculate the numbr of neighbous

--- src/lib.rs
+++ src/lib.rs
@@ -18,11 +18,38 @@ fn should_spawn(number_of_neighbours: u32) -> bool {
     number_of_neighbours == 3
 }
 
+/// This function calculates the distance vector of two places.
+///
+/// ```text
+///                      ->   / b1 - a1 \
+/// A(a1, a2), B(b1, b2) AB = |         |
+///                           \ b2 - a2 /
+/// ```
+fn connection_vector(a: &Place, b: &Place) -> (isize, isize) {
+    (
+        b.get_x() as isize - a.get_x() as isize,
+        b.get_y() as isize - a.get_y() as isize
+    )
+}
+
+/// This function calculates the distance of two places.
+fn distance(a: &Place, b: &Place) -> f64 {
+    let (a, b) = connection_vector(a, b);
+    let powered_sum = a.pow(2) + b.pow(2);
+
+    (powered_sum as f64).sqrt()
+}
+
+/// This struct describes the status of a cell population.
 #[derive(Debug, PartialEq, Clone)]
 pub struct Status {
+    /// The iteration of in which the population was.
     iteration: usize,
+    /// Number of cells the population has.
     cells: usize,
+    /// How many cells were born in comparison to the previous iteration.
     born: usize,
+    /// How many cells were died in comparison to the previous iteration.
     died: usize,
 }
 
@@ -46,6 +73,14 @@ impl Status {
 
     fn get_cells(&self) -> usize {
         self.cells
+    }
+
+    fn get_born(&self) -> usize {
+        self.born
+    }
+
+    fn get_died(&self) -> usize {
+        self.died
     }
 }
 
@@ -78,6 +113,18 @@ impl Population {
     }
 
     fn next_generation(&self) -> Population {
+        for y in 0..self.height {
+            for x in 0..self.width {
+                let current = Place::new(x, y);
+                let number_of_neighbours = 0;//count_neighbours(&self.cells, &current);
+
+                match self.get_cell(&current) {
+                    Some(_) => {},
+                    None => {},
+                }
+            }
+        }
+
         Population {
             iteration: self.iteration + 1,
             width: self.width,
@@ -97,7 +144,9 @@ impl Population {
 
         for y in 0..self.height {
             for x in 0..self.width {
-                match self.get_cell(Place::new(x, y)) {
+                let current = Place::new(x, y);
+
+                match self.get_cell(&current) {
                     Some(_) => buf.push('O'),
                     None => buf.push(' '),
                 }
@@ -109,9 +158,9 @@ impl Population {
         buf
     }
 
-    fn get_cell(&self, position: Place) -> Option<Cell> {
+    fn get_cell(&self, position: &Place) -> Option<Cell> {
         let mut it = self.get_cells().into_iter();
-        it.find(|cell| cell.position == position)
+        it.find(|cell| cell.position == *position)
     }
 }
 
@@ -130,6 +179,10 @@ impl Cell {
     pub fn new(position: Place) -> Cell {
         Cell { position }
     }
+
+    fn get_position(&self) -> &Place {
+        &self.position
+    }
 }
 
 #[derive(Debug, PartialEq, Clone)]
@@ -141,6 +194,14 @@ pub struct Place {
 impl Place {
     pub fn new(x: usize, y: usize) -> Place {
         Place { x, y }
+    }
+
+    fn get_x(&self) -> usize {
+        self.x
+    }
+
+    fn get_y(&self) -> usize {
+        self.y
     }
 }
 
@@ -240,6 +301,112 @@ mod tests {
     }
 
     #[test]
+    fn count_neighbours_empty_vector() {
+        assert_that!(count_neighbours(&Vec::new(), &Place::new(1, 1)), is(equal_to(0)));
+    }
+
+    #[test]
+    fn test_connection_vector_for_zero_points() {
+        assert_that!(connection_vector(&Place::new(0, 0), &Place::new(0, 0)), is(equal_to((0, 0))));
+    }
+
+    #[test]
+    fn test_connection_vector_for_same_points() {
+        assert_that!(connection_vector(&Place::new(42, 23), &Place::new(42, 23)), is(equal_to((0, 0))));
+    }
+
+    #[test]
+    fn test_connection_vector_for_first_point_is_zero() {
+        assert_that!(connection_vector(&Place::new(0, 0), &Place::new(42, 23)), is(equal_to((42, 23))));
+    }
+
+    #[test]
+    fn test_connection_vector_for_second_point_is_zero() {
+        assert_that!(connection_vector(&Place::new(42, 23), &Place::new(0, 0)), is(equal_to((-42, -23))));
+    }
+
+    #[test]
+    fn test_connection_vector_for_two_different_places() {
+        assert_that!(connection_vector(&Place::new(5, 10), &Place::new(7, 8)), is(equal_to((2, -2))));
+    }
+
+    #[test]
+    fn distance_of_zero_places() {
+        assert_that!(distance(&Place::new(0, 0), &Place::new(0 ,0)), is(equal_to(0.0)));
+    }
+
+    #[test]
+    fn distance_of_same_places() {
+        assert_that!(distance(&Place::new(3, 3), &Place::new(3 ,3)), is(equal_to(0.0)));
+    }
+
+    #[test]
+    fn distance_of_two_neighbours() {
+        assert_that!(distance(&Place::new(5, 5), &Place::new(4, 4)), is(close_to(1.4142, 0.0001)));
+        assert_that!(distance(&Place::new(5, 5), &Place::new(5, 4)), is(close_to(1.0, 0.0001)));
+        assert_that!(distance(&Place::new(5, 5), &Place::new(6, 4)), is(close_to(1.4142, 0.0001)));
+        assert_that!(distance(&Place::new(5, 5), &Place::new(4, 5)), is(close_to(1.0, 0.0001)));
+        assert_that!(distance(&Place::new(5, 5), &Place::new(6, 5)), is(close_to(1.0, 0.0001)));
+        assert_that!(distance(&Place::new(5, 5), &Place::new(4, 6)), is(close_to(1.4142, 0.0001)));
+        assert_that!(distance(&Place::new(5, 5), &Place::new(5, 6)), is(close_to(1.0, 0.0001)));
+        assert_that!(distance(&Place::new(5, 5), &Place::new(6, 6)), is(close_to(1.4142, 0.0001)));
+    }
+
+    #[test]
+    fn distance_of_two_not_neighbours() {
+        assert_that!(distance(&Place::new(5, 5), &Place::new(3, 3)), is(close_to(2.8284, 0.0001)));
+        assert_that!(distance(&Place::new(5, 5), &Place::new(4, 3)), is(close_to(2.2360, 0.0001)));
+        assert_that!(distance(&Place::new(5, 5), &Place::new(5, 3)), is(close_to(2.0, 0.0001)));
+        assert_that!(distance(&Place::new(5, 5), &Place::new(6, 3)), is(close_to(2.2360, 0.0001)));
+        assert_that!(distance(&Place::new(5, 5), &Place::new(7, 3)), is(close_to(2.8284, 0.0001)));
+        assert_that!(distance(&Place::new(5, 5), &Place::new(3, 4)), is(close_to(2.2360, 0.0001)));
+        assert_that!(distance(&Place::new(5, 5), &Place::new(7, 4)), is(close_to(2.2360, 0.0001)));
+        assert_that!(distance(&Place::new(5, 5), &Place::new(3, 5)), is(close_to(2.0, 0.0001)));
+        assert_that!(distance(&Place::new(5, 5), &Place::new(7, 5)), is(close_to(2.0, 0.0001)));
+        assert_that!(distance(&Place::new(5, 5), &Place::new(3, 6)), is(close_to(2.2360, 0.0001)));
+        assert_that!(distance(&Place::new(5, 5), &Place::new(7, 7)), is(close_to(2.8284, 0.0001)));
+        assert_that!(distance(&Place::new(5, 5), &Place::new(3, 7)), is(close_to(2.8284, 0.0001)));
+        assert_that!(distance(&Place::new(5, 5), &Place::new(4, 7)), is(close_to(2.2360, 0.0001)));
+        assert_that!(distance(&Place::new(5, 5), &Place::new(5, 7)), is(close_to(2.0, 0.0001)));
+        assert_that!(distance(&Place::new(5, 5), &Place::new(6, 7)), is(close_to(2.2360, 0.0001)));
+        assert_that!(distance(&Place::new(5, 5), &Place::new(7, 7)), is(close_to(2.8284, 0.0001)));
+    }
+
+    /// This function counts the number of neighbours (living cells) for a given place.
+    fn count_neighbours(cells: &Vec<Cell>, position: &Place) -> usize {
+        let mut neighbours = 0;
+
+        for cell in cells {
+            let distance = distance(cell.get_position(), position);
+
+            if distance <= 1.0 {
+                neighbours += 1;
+            }
+        }
+
+        neighbours
+    }
+
+    #[test]
+    #[ignore]
+    fn count_neighbours_surrounded_by_eight_neighbours() {
+        // (4,4) (5,4) (6,4)
+        // (4,5) [5,5] (6,5)
+        // (4,6) (5,6) (6,6)
+        let cells = vec![
+            Cell::new(Place::new(4, 4)),
+            Cell::new(Place::new(5, 4)),
+            Cell::new(Place::new(6, 4)),
+            Cell::new(Place::new(4, 5)),
+            Cell::new(Place::new(6, 5)),
+            Cell::new(Place::new(4, 6)),
+            Cell::new(Place::new(5, 6)),
+            Cell::new(Place::new(6, 6))
+        ];
+        assert_that!(count_neighbours(&cells, &Place::new(5, 5)), is(equal_to(8)));
+    }
+
+    #[test]
     fn format_game_status() {
         let status = Status::new(42, 23, 5, 3);
 
@@ -268,7 +435,17 @@ mod tests {
 
     #[test]
     #[ignore]
-    fn generate_next_population_single_cell_must_die() {}
+    fn generate_next_population_single_cell_must_die() {
+        let cells: Vec<Cell> = vec![
+            Cell::new(Place::new(9, 4))
+        ];
+        let sut = Population::new(10, 5, cells);
+
+        let next = sut.next_generation().get_status();
+
+        assert_that!(next.get_cells(), is(equal_to(0)));
+        assert_that!(next.get_died(), is(equal_to(1)));
+    }
 
     #[test]
     #[ignore]
@@ -316,7 +493,7 @@ mod tests {
 
     #[test]
     fn format_some_population() {
-        let cells: Vec<Cell> = vec!(
+        let cells: Vec<Cell> = vec![
             Cell::new(Place::new(0, 0)),
             Cell::new(Place::new(9, 0)),
             Cell::new(Place::new(0, 1)),
@@ -333,7 +510,7 @@ mod tests {
             Cell::new(Place::new(9, 3)),
             Cell::new(Place::new(0, 4)),
             Cell::new(Place::new(9, 4))
-        );
+        ];
         let sut = Population::new(10, 5, cells);
         let expected = r#"Iteration: 0, Cells: 16, Born: 0, Died: 0
 O        O
@@ -350,7 +527,7 @@ O        O
     fn get_cell_not_found() {
         let sut = Population::new(5, 5, Vec::new());
 
-        assert_that!(sut.get_cell(Place::new(1, 1)), is(equal_to(None)));
+        assert_that!(sut.get_cell(&Place::new(1, 1)), is(equal_to(None)));
     }
 
     #[test]
@@ -358,20 +535,20 @@ O        O
         let sut = Population::new(
             5,
             5,
-            vec!(Cell::new(Place::new(1, 1))));
+            vec![Cell::new(Place::new(1, 1))]);
 
-        assert_that!(sut.get_cell(Place::new(1, 1)), is(equal_to(Some(Cell::new(Place::new(1, 1))))));
+        assert_that!(sut.get_cell(&Place::new(1, 1)), is(equal_to(Some(Cell::new(Place::new(1, 1))))));
     }
 
     #[test]
     fn get_number_of_cells_in_status() {
-        let cells: Vec<Cell> = vec!(
+        let cells: Vec<Cell> = vec![
             Cell::new(Place::new(0, 0)),
             Cell::new(Place::new(9, 0)),
             Cell::new(Place::new(0, 1)),
             Cell::new(Place::new(2, 1)),
             Cell::new(Place::new(7, 1))
-        );
+        ];
 
         let sut = Population::new(10, 5, cells);
 

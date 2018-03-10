5c4716d0569cea3973635f88779160937b9c0ee6
Sven Strittmatter <sven.strittmatter@iteratec.de>
2018-03-10 11:26:58 +0100

    Improve error handling for CLI options

--- src/config.rs
+++ src/config.rs
@@ -14,7 +14,7 @@ pub static DEFAULT_RATIO: &'static str = "4";
 
 /// Creates a new config from the arguments matcher.
 /// This function validates the values and throws an error if not met requirements.
-pub fn create_config(matches: &ArgMatches) -> Result<Config, &'static str>  {
+pub fn create_config(matches: &ArgMatches) -> Result<Config, String> {
     let width = matches.value_of("width").unwrap_or(DEFAULT_WIDTH);
     let height = matches.value_of("height").unwrap_or(DEFAULT_HEIGHT);
     let sleep = matches.value_of("sleep").unwrap_or(DEFAULT_SLEEP);
@@ -23,20 +23,62 @@ pub fn create_config(matches: &ArgMatches) -> Result<Config, &'static str>  {
     validate_config(width, height, sleep, ratio)
 }
 
-fn validate_config(width: &str, height: &str, sleep: &str, ratio: &str) -> Result<Config, &'static str> {
-    let width = width.parse::<usize>().expect("Not negative number expected as width!");
-    // TODO check for minimum size.
+fn validate_config(width: &str, height: &str, sleep: &str, ratio: &str) -> Result<Config, String> {
+    let width = match width.parse::<usize>() {
+        Ok(w) => {
+            if w < 1 {
+                return Err(error_bad_option_to_small("width", 1))
+            }
 
-    let height = height.parse::<usize>().expect("Not negative number expected as height!");
-    // TODO check for minimum size.
+            w
+        },
+        Err(_) => return Err(error_bad_option_not_number("width")),
+    };
 
-    let sleep = sleep.parse::<u64>().expect("Not negative number expected as sleep!");
-    // TODO Check for not less than 1
+    let height = match height.parse::<usize>() {
+        Ok(h) => {
+            if h < 1 {
+                return Err(error_bad_option_to_small("height", 1))
+            }
 
-    let ratio = ratio.parse::<u32>().expect("Not negative number expected as sleep!");
-    // TODO Check for not less than 1
+            h
+        },
+        Err(_) => return Err(error_bad_option_not_number("height")),
+    };
+
+    let sleep = match sleep.parse::<u64>() {
+        Ok(s) => {
+            if s < 1 {
+                return Err(error_bad_option_to_small("sleep", 1))
+            }
+
+            s
+        },
+        Err(_) => return Err(error_bad_option_not_number("sleep")),
+    };
+
+    let ratio = match ratio.parse::<u32>() {
+        Ok(r) => {
+            if r < 1 {
+                return Err(error_bad_option_to_small("ratio", 1))
+            }
+
+            r
+        },
+        Err(_) => return Err(error_bad_option_not_number("ratio")),
+    };
 
     Ok(Config::new(width, height, sleep, ratio))
+}
+
+static OPTION_ERROR_PREFIX: &'static str = "Bad option:";
+
+fn error_bad_option_not_number(name: &str) -> String {
+    format!("{} Not negative number expected as option '--{}'!", OPTION_ERROR_PREFIX, name)
+}
+
+fn error_bad_option_to_small(name: &str, min: usize) -> String {
+    format!("{} Too small value for option '--{}' given! Minimum is {}.", OPTION_ERROR_PREFIX, name, min)
 }
 
 /// This struct holds the configuration for the game.
@@ -92,5 +134,98 @@ mod tests {
         assert_that!(
             format!("{}", sut),
             is(equal_to(String::from("Width:        42, Height:    23, Sleep:     5, Ration:     3"))));
+    }
+
+    #[test]
+    fn validate_config_width_is_not_usize() {
+        let result = validate_config("foo", "1", "1", "1");
+
+        assert_that!(
+            result,
+            is(equal_to(Err(
+                String::from("Bad option: Not negative number expected as option '--width'!")))));
+    }
+
+    #[test]
+    fn validate_config_width_is_too_small() {
+        let result = validate_config("0", "1", "1", "1");
+
+        assert_that!(
+            result,
+            is(equal_to(Err(
+                String::from("Bad option: Too small value for option '--width' given! Minimum is 1.")))));
+    }
+
+    #[test]
+    fn validate_config_height_is_not_usize() {
+        let result = validate_config("1", "foo", "1", "1");
+
+        assert_that!(
+            result,
+            is(equal_to(Err(
+                String::from("Bad option: Not negative number expected as option '--height'!")))));
+    }
+
+    #[test]
+    fn validate_config_height_is_to_small() {
+        let result = validate_config("1", "0", "1", "1");
+
+        assert_that!(
+            result,
+            is(equal_to(Err(
+                String::from("Bad option: Too small value for option '--height' given! Minimum is 1.")))));
+    }
+
+    #[test]
+    fn validate_config_sleep_is_not_usize() {
+        let result = validate_config("1", "1", "foo", "1");
+
+        assert_that!(
+            result,
+            is(equal_to(Err(
+                String::from("Bad option: Not negative number expected as option '--sleep'!")))));
+    }
+
+    #[test]
+    fn validate_config_sleep_is_to_small() {
+        let result = validate_config("1", "1", "0", "1");
+
+        assert_that!(
+            result,
+            is(equal_to(Err(
+                String::from("Bad option: Too small value for option '--sleep' given! Minimum is 1.")))));
+    }
+
+    #[test]
+    fn validate_config_ratio_is_not_usize() {
+        let result = validate_config("1", "1", "1", "foo");
+
+        assert_that!(
+            result,
+            is(equal_to(Err(
+                String::from("Bad option: Not negative number expected as option '--ratio'!")))));
+    }
+
+    #[test]
+    fn validate_config_ratio_is_to_small() {
+        let result = validate_config("1", "1", "1", "0");
+
+        assert_that!(
+            result,
+            is(equal_to(Err(
+                String::from("Bad option: Too small value for option '--ratio' given! Minimum is 1.")))));
+    }
+
+    #[test]
+    fn validate_config_with_sane_values() {
+        let result = match validate_config("1", "2", "3", "4") {
+            Ok(r) => r,
+            Err(_) => panic!("Expected Ok(Config) as result!"),
+        };
+
+        assert_that!(result.get_width(), is(equal_to(1)));
+        assert_that!(result.get_height(), is(equal_to(2)));
+        assert_that!(result.get_sleep(), is(equal_to(time::Duration::from_secs(3))));
+        assert_that!(result.get_ratio(), is(equal_to(4)));
     }
 }
